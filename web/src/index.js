fd45c84f427d1e758be67b8877d26538d1b2be40
cgons <cgons@pcxchange.ca>
2019-04-25 10:38:13 -0400

    WIP

--- web/src/index.js
+++ web/src/index.js
@@ -13,7 +13,8 @@ class App extends React.Component {
         <nav className="row site-header">
           <h3 className="col site-title">GO Refund App</h3>
           <div className="col sign-in">
-            <button className="unstyled"><i class="fab fa-google"></i> <span>Sign in</span></button>
+            {/* <button className="unstyled"><i className="fab fa-google"></i> <span>Sign in</span></button> */}
+            <div className="g-signin2" data-onsuccess="onSignIn"></div>
           </div>
         </nav>
         <div>
@@ -27,3 +28,12 @@ class App extends React.Component {
 
 
 ReactDOM.render(<App />, document.getElementById("app"));
+
+function onSignIn(googleUser) {
+  var profile = googleUser.getBasicProfile();
+  console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+  console.log('Name: ' + profile.getName());
+  console.log('Image URL: ' + profile.getImageUrl());
+  console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
+}
+window.onSignIn = onSignIn;
--- web/src/routes/Home/Home.js
+++ web/src/routes/Home/Home.js
@@ -16,6 +16,42 @@ export default class Home extends React.Component {
               Get Started
             </a>
           </div>
+
+          <div className="link-content">
+            <a href="">How it works</a>
+          </div>
+        </section>
+
+        <section className="links row">
+          <a href="">
+            <i className="fas fa-train"></i>
+            <h4>Check GO Trip Status</h4>
+          </a>
+
+          <a href="">
+            <i className="fas fa-hand-holding-usd"></i>
+            <h4>View Eligible Refunds</h4>
+          </a>
+        </section>
+
+        <section className="how-it-works">
+          <h2 className="title">How it works</h2>
+
+          <div className="sub-section">
+            <h3 className="title left">one</h3>
+
+            <div className="text-content left">
+              Sign in (with your Google account).
+            </div>
+          </div>
+
+          <div className="sub-section">
+            <h3 className="title right">one</h3>
+
+            <div className="text-content right">
+              Sign in (with your Google account).
+            </div>
+          </div>
         </section>
       </div>
       );
